4429f245f35a1d157212b5ec0812e8b726e1d638
Nakamura Takahiro <takahiro.nakamura@gi-no.jp>
2017-12-08 23:09:05 +0900

    try lambda with dynamodb
    via https://qiita.com/noralife/items/e36621ddd0e5b8ff4447

--- handler.js
+++ handler.js
@@ -1,16 +1,65 @@
-'use strict';
+"use strict"
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+var AWS = require("aws-sdk");
 
-  callback(null, response);
+var judgeJanken = (a, b) => {
+  var c = (a - b + 3) % 3;
+  if (c === 0) {
+    return "draw";
+  } else if (c === 2) {
+    return "win";
+  } else {
+    return "lose";
+  }
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+var getDynamoClient = (event) => {
+  var options = {};
+  if ("isOffline" in event && event.isOffline) {
+    options = ({
+      region: "localhost",
+      endpoint: "http://localhost:8001",
+    });
+  }
+  return new AWS.DynamoDB.DocumentClient(options);
+}
+
+ module.exports.playJanken = (event, context, callback) => {
+   console.log(`received event: ${ JSON.stringify(event, null, 2) }`);
+   console.log(`received context: ${ JSON.stringify(context, null, 2) }`);
+
+   var dynamodb = getDynamoClient(event);
+   var date = new Date();
+   var unixtime = Math.floor(date.getTime() / 1000);
+
+   var hand = ['rock', 'sissors', 'paper'];
+   var player_name = event.queryStringParameters.name;
+   var player_hand = event.queryStringParameters.hand;
+   var player = hand.indexOf(player_hand);
+   var computer = Math.floor(Math.random() * 3);
+   var judge = judgeJanken(player, computer);
+
+   var params = {
+     TableName: 'jankens',
+     Item: {
+       player: player_name,
+       unixtime: unixtime,
+       player_hand: player_hand,
+       computer_hand: hand[computer],
+       judge: judge,
+     }
+   };
+
+   dynamodb.put(params, (error) => {
+     var response = { statusCode: null, body: null };
+     if (error) {
+       console.log(error);
+       response.statusCode = 500;
+       response.body = { code: 500, message: 'failed to put' }
+     } else {
+       response.statusCode = 200;
+       response.body = JSON.stringify(params.Item)
+     }
+     callback(null, response)
+   });
+ };
