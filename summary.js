9acf9f6ae6c728df358f6da8c788cf2151cb8f22
Nakamura Takahiro <takahiro.nakamura@gi-no.jp>
2017-12-18 11:02:14 +0900

    make summary script and change key

--- summary.js
+++ summary.js
@@ -16,33 +16,27 @@ var getDynamoClient = (event) => {
 
  module.exports.run = (event, context, callback) => {
     var ddb = getDynamoClient(event);
-
-    var date = new Date();
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    var unixtime = Math.floor(date.getTime() / 1000);
     var params = qs.parse(event.body);
 
-    var data = {
+    var query = {
       TableName: 'Orders',
-      Item: {
-        User: params.user_id,
-        Unixtime: unixtime,
-        YearMonth: parseInt(`${year}${month}`),
-        Name: params.user_name,
+      IndexName: 'YearMonthIndex',
+      ExpressionAttributeValues: {
+        ':ym': 201712,
       },
+      KeyConditionExpression: 'YearMonth = :ym',
     };
 
-    ddb.put(data, (error) => {
-      var response = { statusCode: null, body: null };
+    ddb.query(query, (error, data) => {
+      var response = {};
       if (error) {
         console.log(error);
         response.statusCode = 500;
-        response.body = { code: 500, message: 'failed to put' }
+        response.body = { code: 500, message: 'failed to query' }
       } else {
         response.statusCode = 200;
-        response.body = JSON.stringify(data.Item)
+        response.body = JSON.stringify(data.Items)
       }
       callback(null, response)
-    });
+    })
  };
